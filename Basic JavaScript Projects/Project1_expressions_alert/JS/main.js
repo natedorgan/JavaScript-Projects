8dcbbd095eef257334a31f45a914b04f0da46d20
natedorgan <natedorgan@gmail.com>
2022-09-01 14:04:31 -0700

    Basic Javascript Project Submission Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,4 @@
+var String1 = "This is the beginning of the string", String2 = " and the end of the string"; //Defining vars Script1 and Script 2
+document.write(String1 + String2);                                                           //Displaying Script1 + Script2 Concatenated
+window.alert(4 + 5);                                                                         //Popup with 9 
+
